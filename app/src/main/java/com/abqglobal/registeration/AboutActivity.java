16cab57b5671ebcf31571745b13ecc4839f5d526
Ashwani <mishraashwani325@gmail.com>
2019-01-11 15:59:16 +0530

    about activity

--- app/src/main/java/com/abqglobal/registeration/AboutActivity.java
+++ app/src/main/java/com/abqglobal/registeration/AboutActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class AboutActivity extends AppCompatActivity {
-Button bl;
+Button bl;  
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
