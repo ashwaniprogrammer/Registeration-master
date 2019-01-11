dccb82781357d424ec1d6c0d3968726cce4d546a
Ashwani <mishraashwani325@gmail.com>
2019-01-11 16:01:41 +0530

    about activity

--- app/src/main/java/com/abqglobal/registeration/Main2Activity.java
+++ app/src/main/java/com/abqglobal/registeration/Main2Activity.java
@@ -2,13 +2,12 @@ package com.abqglobal.registeration;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class AboutActivity extends AppCompatActivity {
-Button bl;
+public class Main2Activity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_main2);
     }
 }
